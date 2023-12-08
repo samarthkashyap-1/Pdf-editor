c569a44f2e18aac677b573d54c9fdd099370cc74
samarth kashyap <samarthkashyap318@gmail.com>
2023-12-08 20:58:12 +0530

    watermark fixed

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ const Pdfmodifier = () => {
           width: watermarkDimensions.width,
           height: watermarkDimensions.height,
           color: rgb(1, 1, 1),
-          opacity: 0.1,
+          opacity: 0.3,
           rotate: degrees(40),
         });
       });
